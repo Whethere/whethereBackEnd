47bd6a63eae489d989ad08cea73bf91bb09af55d
jieunee2 <980lje@naver.com>
2022-02-18 19:51:56 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/whethere/WhethereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.whethere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhethereBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WhethereBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/whethere/WhethereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whethere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhethereBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
